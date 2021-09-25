955c2d5a2573e7e72a0b40df32fd27c249b9c61d
Milen Penev <milennpenev@gmail.com>
2021-09-25 20:55:57 +0300

    exercise

---
+++ 01.syntax/previousDate.js
@@ -0,0 +1,9 @@
+function previousDate(year, month, day) {
+    let dateString = year + '-' + month + '-' + day;
+    let event = new Date(dateString);
+    event.setDate(day-1);
+    console.log(event.getFullYear() + `-` + (Number(event.getMonth())+1) + '-' + event. getDate());
+    
+}
+
+previousDate(2016, 9, 30)
---
+++ 01.syntax/sameNumbers.js
@@ -0,0 +1,17 @@
+function numbers(intNum) {
+    const strNum = intNum.toString();
+    let result = parseInt(strNum[0])
+    let isSame = true;
+
+    for (let i=1; i < strNum.length; i++) {
+        result += parseInt(strNum[i])
+
+        if (strNum[i] != strNum[i-1]) {
+            isSame = false;
+        }
+    }
+console.log(isSame);
+console.log(result);
+}
+
+numbers(2222222)
