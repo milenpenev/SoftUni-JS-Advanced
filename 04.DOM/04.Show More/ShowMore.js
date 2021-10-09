e266c27713c2d8ebbd604ca69a2b1973df492b16
Milen Penev <milennpenev@gmail.com>
2021-10-09 23:44:56 +0300

    lecture

---
+++ 04.DOM/04.Show More/ShowMore.js
@@ -0,0 +1,6 @@
+function showText() {
+    const text = document.getElementById('text');
+    text.style.display = 'inline';
+    const btn = document.getElementById('more');
+    btn.style.display = 'none'
+}
