8e4d17b1cdf9906d67dc626f4ad13f160e0b4265
Milen Penev <milennpenev@gmail.com>
2021-10-12 18:07:48 +0300

    Lecture

--- 05.DOM Manipulations and Events/02. Delete-From-Table/app.js
+++ 05.DOM Manipulations and Events/02. Delete-From-Table/app.js
@@ -1,3 +1,16 @@
 function deleteByEmail() {
-    console.log('TODO:...');
+    const searchedEmail = document.querySelector('input[name="email"]');
+    const rows = Array.from(document.querySelector('tbody').children);
+    let removed = false;
+    for (let row of rows) {
+        if (row.children[1].textContent == searchedEmail.value) {
+            row.remove();
+            removed = true;
+        }
+    }
+    if (removed){
+        document.getElementById('result').textContent = 'Deleted.'
+    } else {
+        document.getElementById('result').textContent = 'Not Found.'
+    }
 }
