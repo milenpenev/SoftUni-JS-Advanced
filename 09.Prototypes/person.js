478cf39819a6f3641d51b1a348586d35e548d052
Milen Penev <milennpenev@gmail.com>
2021-11-20 11:58:45 +0200

    exercise

--- 09.Prototypes/person.js
+++ 09.Prototypes/person.js
@@ -1,24 +1,23 @@
 function createPerson(firstName, lastName) {
-    return {
-        firstName,
+    const result = {firstName,
         lastName,
         fullName: `${firstName} ${lastName}`
-    }
+    };
+    Object. defineProperty(result, 'fullName', {
+        get() {
+            return `${this.firstName} ${this.lastName}`
+        },
+        set(value){
+            const [first, last] = value.split(' ');
+            if (first != undefined && last != undefined){
+                this.firstName = first;
+                this.lastName = last;
+            }
+            
+        },
+        configurable: true,
+        enumerable: true
+    })
+        return result
 }
 
-let person = new Person("Peter", "Ivanov");
-console.log(person.fullName); //Peter Ivanov
-person.firstName = "George";
-console.log(person.fullName); //George Ivanov
-person.lastName = "Peterson";
-console.log(person.fullName); //George Peterson
-person.fullName = "Nikola Tesla";
-console.log(person.firstName); //Nikola
-console.log(person.lastName); //Tesla
-let person = new Person("Albert", "Simpson");
-console.log(person.fullName); //Albert Simpson
-person.firstName = "Simon";
-console.log(person.fullName); //Simon Simpson
-person.fullName = "Peter";
-console.log(person.firstName);  // Simon
-console.log(person.lastName);  // Simpson
